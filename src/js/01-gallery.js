1fde003401954a703aeca9041b2f9e5da6eb8e59
OleksandrNovyk <novykav@gmail.com>
2023-03-09 19:22:43 +0200

    modified:   src/js/01-gallery.js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -30,31 +30,3 @@ function createGalleryItemsMarkup(items) {
     captionDelay: 250,
     close: false,  //show the close button or not
   });
-
-  //2nd
-//   const galleryContainer = document.querySelector('.gallery');
-// // console.log(galleryContainer);
-// const itemsMarkup = createGalleryItemsMarkup(galleryItems);
-// galleryContainer.insertAdjacentHTML('beforeend', itemsMarkup);
-
-// // rendered items
-// function createGalleryItemsMarkup(items) {
-//   return items.map(({ preview, original, description }) => {
-//     return `
-//   <a class="gallery__item" href="${original}">
-//     <img
-//       class="gallery__image"
-//       src="${preview}"
-//       alt="${description}"
-//     />
-//   </a>
-// `
-//   }).join('');
-// }
-// // use library SimpleLightbox
-// const lightbox = new SimpleLightbox('.gallery a', {
-//   captionsData: 'alt', 
-//   captionPosition: 'bottom', 
-//   captionDelay: 250,
-//   close: false,  //show the close button or not
-// });
