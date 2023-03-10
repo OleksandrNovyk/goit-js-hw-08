dbe6bd75125f0fae01ec7525429c8fd086ae6c3b
OleksandrNovyk <novykav@gmail.com>
2023-03-10 14:47:57 +0200

    src/js/03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,6 +1,6 @@
 import throttle from 'lodash.throttle';
 
-const LOCAL_KEY = 'feedback-form-state';
+const LOCAL_KEY = 'feedback-form-state';       //ключ для сховища
 
 const form = document.querySelector('.feedback-form');
 
@@ -9,8 +9,8 @@ form.addEventListener('submit', onFormSubmit);
 
 let dataForm = JSON.parse(localStorage.getItem(LOCAL_KEY)) || {};
 const { email, message } = form.elements;
-console.log(form.elements);
-reloadPage();   //при перезавантаженні сторінки залишає дані у полі email та message(п.2 Task)
+console.log(form.elements);     // виводить у консоль усі елементи форми
+reloadPage();     //при перезавантаженні сторінки залишає дані у полі email та message(п.2 Task)
 
 function onInputData(e) {
   dataForm = { email: email.value, message: message.value };
@@ -27,13 +27,14 @@ function reloadPage() {
 function onFormSubmit(e) {
   e.preventDefault();
 
-  if (email.value === '' || message.value === '') {
-    return alert('Please fill in all the fields!');
+  if (email.value === '' || message.value === '') {                // якщо значення email або message відсутнє,
+    return alert('Please fill in all the fields!');               // то вивиодимо alert Please fill in all the fields!'
   }
-  console.log({ email: email.value, message: message.value });
+  console.log({ email: email.value, message: message.value });   // виводить у консоль email та message тільки у тому випадку, якщо і поле email і поле message заповнені
 
   localStorage.removeItem(LOCAL_KEY);
   e.currentTarget.reset();
+  console.log(e.currentTarget);          // поточний об'єкт(feedback-form)
   dataForm = {};
 }
 
